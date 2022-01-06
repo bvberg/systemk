9fcff32fbd257a826d963373d09aed7dc323abe9
bvanderberg <barend.vanderberg@agoda.com>
2022-01-06 12:28:51 +0200

    changed the path to /images

--- internal/ospkg/image.go
+++ internal/ospkg/image.go
@@ -17,7 +17,7 @@ type ImageManager struct{}
 var _ Manager = (*ImageManager)(nil)
 
 const (
-	imageBaseRootPath = "/opt/systemk/services"
+	imageBaseRootPath = "/opt/systemk/images"
 )
 
 func (p *ImageManager) Install(pkg, version string) (bool, error) {
